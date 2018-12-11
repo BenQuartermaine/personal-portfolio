c04e95b6600ecfe0c3c2b3c73ac88a704271e482
Ben Quartermaine <ben.quartermaine3@gmail.com>
2018-12-11 13:12:16 +0800

    added images folder, beginning to incorporate information into slide out side bar

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Opening and closing of slide-out case studies
 function closeCase() {
   document.getElementById("side-bar").style.width = "0px";
 }
-
 function openCase() {
   document.getElementById("side-bar").style.width = "60%";
 }
+
+// Listens for esc key when viewing slide-out case studies
+document.body.addEventListener("keydown", function (e) {
+  if (e.keyCode == 27) { closeCase() };
+})

8c5551e4b17e8f20251f3bc4a6305f1c8c8300af
Ben Quartermaine <ben.quartermaine3@gmail.com>
2018-12-12 16:49:17 +0800

    added new version of images, noscroll on body

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // Opening and closing of slide-out case studies
 function closeCase() {
-  document.getElementById("side-bar").style.display = "none";
+  const side = document.getElementById('side-bar');
+  const body = document.getElementById('noscroll');
+  side.style.width = "0%"
+  body.classList.remove("noscroll");
 }
 function openCase() {
-  document.getElementById("side-bar").style.display = "block";
+  const sideBar = document.getElementById('side-bar')
+  const body = document.getElementById('noscroll')
+  sideBar.style.width = "60%"
+  body.classList.add("noscroll");
 }
-
 // Listens for esc key when viewing slide-out case studies
 document.body.addEventListener("keydown", function (e) {
   if (e.keyCode == 27) { closeCase() };

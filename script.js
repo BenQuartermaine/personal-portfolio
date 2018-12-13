8785c4ef3349bf7c6a0d976cdc68bb866ebf2034
Ben Quartermaine <ben.quartermaine3@gmail.com>
2018-12-13 14:20:14 +0800

    fixed side-bar expansion bug, now works for mobile (100% width and desktop 60%)

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Opening and closing of slide-out case studies
+const media = window.matchMedia("(max-width: 700px)")
+
 function closeCase() {
   const side = document.getElementById('side-bar');
   const body = document.getElementById('noscroll');
   side.style.width = "0%"
   body.classList.remove("noscroll");
 }
-function openCase() {
+function openCase(media) {
   const sideBar = document.getElementById('side-bar')
   const body = document.getElementById('noscroll')
-  sideBar.style.width = "60%"
+  if (media.matches) {
+    sideBar.style.width = "100%";
+  } else {
+    sideBar.style.width = "60%";
+  };
   body.classList.add("noscroll");
 }
 // Listens for esc key when viewing slide-out case studies

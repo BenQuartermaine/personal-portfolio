07c699c9aa2af7f72d72e331006086ea2ab0c4e8
Ben Quartermaine <ben.quartermaine3@gmail.com>
2018-12-14 18:36:50 +0800

    not sure where this is leading

---
+++ script.js
@@ -0,0 +1,26 @@
+// Opening and closing of slide-out case studies
+function closeCase(id) {
+  const side = document.getElementById(`side-bar-${id}`);
+  const body = document.getElementById('noscroll');
+  side.style.width = "0%";
+  body.classList.remove("noscroll");
+}
+function openCase(id) {
+  const media = window.matchMedia("(max-width: 700px)");
+  const side = document.getElementById(`side-bar-${id}`);
+  const body = document.getElementById('noscroll');
+  if (media.matches) {
+    side.style.width = "100%";
+  } else {
+    side.style.width = "60%";
+  }
+  body.classList.add("noscroll");
+}
+// Listens for esc key when viewing slide-out case studies
+document.body.addEventListener("keydown", function (e) {
+  if (e.keyCode === 27) { 
+    document.getElementById("side-bar-mirror").style.width = "0%";
+    document.getElementById("side-bar-mindrock").style.width = "0%";
+    document.getElementById('noscroll').classList.remove("noscroll");
+  }
+});

a383f01a279b2d6a72d0873fcf025fb19eda2a07
Ben Quartermaine <ben.quartermaine3@gmail.com>
2018-12-14 09:46:16 +0800

    changed the id structure of side-bar to allow for many case studies, added mindrock case study, still needs styling

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Opening and closing of slide-out case studies
 const media = window.matchMedia("(max-width: 700px)")
 
-function closeCase() {
-  const side = document.getElementById('side-bar');
+function closeCase(id) {
+  const side = document.getElementById(`side-bar-${id}`);
   const body = document.getElementById('noscroll');
   side.style.width = "0%"
   body.classList.remove("noscroll");
 }
-function openCase(media) {
-  const sideBar = document.getElementById('side-bar')
+function openCase(media, id) {
+  const side = document.getElementById(`side-bar-${id}`);
   const body = document.getElementById('noscroll')
   if (media.matches) {
-    sideBar.style.width = "100%";
+    side.style.width = "100%";
   } else {
-    sideBar.style.width = "60%";
+    side.style.width = "60%";
   };
   body.classList.add("noscroll");
 }
